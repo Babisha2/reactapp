a925703496f3c6e6ebf1785790699ae827a95a10
Babisha2 <babishamol735@gmail.com>
2025-08-01 20:56:31 +0530

    Fix: case-sensitive import for Carousel

--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';;
 import Navbar from '../components/Navbar'
-import Carousel from '../components/carousel';
+import Carousel from '../components/Carousel';
 import Container from '../components/Container';
 import Banner from  '../components/Banner';
 import Section from  '../components/Section';
